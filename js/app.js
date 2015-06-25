31cfcebe4e160198c97a96e8fc62a6253c64e465
dannysinger <danieljosephsinger@gmail.com>
2015-06-25 16:36:30 -0400

    basic outline

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-$(document).ready(function() {/*Ryu Standing Still*/});
+$(document).ready(function() {
+    Listen ON Document
+    show("RyuStandingStill");
+    $ WHEN "MouseEnterEvent" show("RyuReadyPose")
+    $ WHEN "MouseLeaveEvent" show("RyuStill")
+    $ WHEN "MouseDownEvent" show("RyuThrowPose") 
+        AND show("HADOUKEN-moving-x"), hide("AnythingElse"), playSound("Hadouken")
+    $ WHEN "MouseUpEvent" show("RyuStill"), hide("AnythingElse")
+    $ WHEN "KeyDownPressX" show("COOL"), hide("AnythingElse")
+    $ WHEN "KeyUpPressX"  show("RyuStill"), hide("AnythingElse")}
+                
